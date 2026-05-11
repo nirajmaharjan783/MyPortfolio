96a4001956f6ebc7d9c1d51b9b292eaf59dd421e
Niraj <nirajmaharjan783@gmail.com>
2026-05-12 01:13:03 +0545

    Show live demo button on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
                       ))}
                     </div>
 
-                    <div className="mt-6 flex flex-col sm:flex-row sm:flex-wrap gap-4">
+                    <div className="mt-6 flex flex-col gap-4 sm:flex-row sm:flex-wrap">
                       <a
                         href={project.github}
                         target="_blank"
